0aef978c4a6420931bf3aeacee294543db138853
sarahstxs <rebeccasarah.book2@gmail.com>
2026-09-17 07:05:00 -0300

    feat: Background on initial page

--- app/src/main/java/com/example/nexusappxml/ui/activity/InitialActivity.kt
+++ app/src/main/java/com/example/nexusappxml/ui/activity/InitialActivity.kt
@@ -4,9 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.bumptech.glide.Glide
 import com.example.nexusappxml.R
 import com.example.nexusappxml.data.local.TokenManager
 import com.example.nexusappxml.ui.view.CoinView
@@ -21,6 +26,25 @@ class InitialActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_initial)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+
+        // Esconde a barra de navegação
+        controller.hide(WindowInsetsCompat.Type.navigationBars())
+
+        // Faz com que a barra apareça apenas se o usuário arrastar de baixo para cima, e depois suma de novo
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        var imgBackground = findViewById<ImageView>(R.id.imgBackground)
+
+        Glide.with(this)
+            .load("https://i.pinimg.com/736x/a7/2a/02/a72a022f37c47d8d294e85577737f362.jpg")
+            .centerCrop()
+            .into(imgBackground)
+
+
+
 
         val perfilPreview = findViewById<PerfilPreviewView>(R.id.user_preview_component)
         val navBar: CustomNavBarView = findViewById(R.id.nav_bar_customizada)
@@ -84,8 +108,7 @@ class InitialActivity : AppCompatActivity() {
         btnGoDeckPage.GotoDeckPage()
 
         // Botão de sair/voltar para o login
-        val formUsernameEmail = findViewById<Button>(R.id.exit)
-        formUsernameEmail.setOnClickListener { backToLogin() }
+        perfilPreview.setOnClickListener { backToLogin() }
     }
 
     fun backToLogin() {
