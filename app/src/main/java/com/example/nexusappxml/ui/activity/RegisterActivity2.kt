32d6d285bdb574dd10ef5e38b879e43a986a97c4
sarahstxs <rebeccasarah.book2@gmail.com>
2026-09-17 08:39:36 -0300

    feat: Add ackground and add personality font to the Register page

--- app/src/main/java/com/example/nexusappxml/ui/activity/RegisterActivity2.kt
+++ app/src/main/java/com/example/nexusappxml/ui/activity/RegisterActivity2.kt
@@ -7,11 +7,16 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 import com.example.nexusappxml.R
 import com.example.nexusappxml.data.model.RegisterRequest
 import com.example.nexusappxml.data.network.ApiService
@@ -28,6 +33,22 @@ class RegisterActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register2)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+
+        // Esconde a barra de navegação
+        controller.hide(WindowInsetsCompat.Type.navigationBars())
+
+        // Faz com que a barra apareça apenas se o usuário arrastar de baixo para cima, e depois suma de novo
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        var imgBackground = findViewById<ImageView>(R.id.imgBackground)
+
+        Glide.with(this)
+            .load("https://i.pinimg.com/736x/a7/2a/02/a72a022f37c47d8d294e85577737f362.jpg")
+            .centerCrop()
+            .into(imgBackground)
 
         // Botão para a tela de login
         val goToLogin = findViewById<Button>(R.id.btnGoToLogin)
